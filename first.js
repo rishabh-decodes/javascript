5cf61eb8302024d6d3cf060ff7d7f21e04dd3827
rishabh-decodes <rishabhsarraf5@gmail.com>
2024-09-24 01:26:21 +0530

    firstCommit

---
+++ first.js
@@ -0,0 +1,28 @@
+       //   Variables
+
+ /*
+    let, Const & var
+
+var : Variable can be re-declared & updated. A global scope.
+
+let : Variable cannot be re-declared but cannot be updated. A block scope.
+
+const : Variable cannot be re-declared or updated. A block scope.
+
+/*
+
+
+
+    //  Data types
+
+
+/* 
+  
+   Number, String, Boolean, Undefined, Null, BigInt, Symbol
+
+     primitive(7)             
+     non-primitive{ objects- ( array, functions) }
+        objects are collection of values                            
+                                
+
+/*
