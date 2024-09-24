64d0ddef85af7c563b87252ea3c37e9747b318b3
rishabh-decodes <rishabhsarraf5@gmail.com>
2024-09-24 21:18:49 +0530

    firstCode

--- first.js
+++ first.js
@@ -1,28 +1,7 @@
-       //   Variables
+// In this code we will print our first javascript code.
+// Hello world!
 
- /*
-    let, Const & var
+// alert("hello world!")
+console.log("hello world!")
 
-var : Variable can be re-declared & updated. A global scope.
-
-let : Variable cannot be re-declared but cannot be updated. A block scope.
-
-const : Variable cannot be re-declared or updated. A block scope.
-
-/*
-
-
-
-    //  Data types
-
-
-/* 
-  
-   Number, String, Boolean, Undefined, Null, BigInt, Symbol
-
-     primitive(7)             
-     non-primitive{ objects- ( array, functions) }
-        objects are collection of values                            
-                                
-
-/*
+//console.log is written to see the output of the code in console.
