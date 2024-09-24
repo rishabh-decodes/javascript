fb00206df0ba9b949f24e4280588716809f872fb
rishabh-decodes <rishabhsarraf5@gmail.com>
2024-09-24 21:35:42 +0530

    variableExampleAdded

---
+++ variable.js
@@ -0,0 +1,27 @@
+       //   Variables
+
+ /*
+    let, Const & var
+
+var : Variable can be re-declared & updated. A global scope.
+
+let : Variable cannot be re-declared but cannot be updated. A block scope.
+
+const : Variable cannot be re-declared or updated. A block scope.
+
+*/
+
+let message = "Hello javascript":  // this is how we assign the value in variable.
+
+alert(message);  //This will print "Hello javascript" in console"
+
+
+//we can also acess variables by their name
+
+let message;
+message = "Hello javascript";
+
+alert(message);  //Prints the variable content.
+
+//for better code practice we should end our code with semi-colon(;)
+
