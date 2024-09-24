7ec3d1cd996d072594436c4f8a5cce0a6d46f71b
rishabh-decodes <rishabhsarraf5@gmail.com>
2024-09-24 22:25:55 +0530

    dataTypesAdded

---
+++ datatypes.js
@@ -0,0 +1,56 @@
+   //  Data types
+
+/* 
+  
+   Number, String, Boolean, Undefined, Null, BigInt, Symbol
+
+     primitive(7)             
+     non-primitive{ objects- ( array, functions) }
+        objects are collection of values                            
+                                
+
+*/
+
+// Strings:
+ let firstName = "Rishabh";
+ let lastName = "Kumar";
+
+// Numbers:
+
+let length = 20;
+let height = 15;
+
+// Booleans:
+
+let x = true;
+let y = false;
+
+// Undefined
+
+let name;     //value of name is not defined.
+
+//Null:
+
+let age = null;  //Null is an assignment value that reprents no value or no object.
+
+//Bigint:
+
+let x = BigInt("1234567890123456789");   //Bigint variavle are used to store big integer values that are too big to be represented.
+
+//Array:
+
+//Array items are writtenn with square brackets.
+//Array items are separated by commas.
+
+let items = ["Mango", "Apples", "Grapes"];
+
+//Objects:
+
+//Objects are written with curly braces{}
+
+let student = {
+    firstName: "Rishabh",
+    lastName: "Kumar",
+    age: 24,
+};
+
